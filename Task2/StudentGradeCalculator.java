ae3c6ebcdbfa4212c9abb87411ee433260e93132
Rachanabshetty <157868474+Rachanabshetty@users.noreply.github.com>
2024-02-12 13:15:13 +0530

    Task2
    StudentGradeCalculator using java

---
+++ Task2/StudentGradeCalculator.java
@@ -0,0 +1,74 @@
+package Task2;
+
+import java.util.Scanner;
+
+
+public class StudentGradeCalculator {
+	
+	 public static void main(String[] args) {
+
+	        Scanner scanner = new Scanner(System.in);
+
+	        System.out.print("\nEnter the number of Subjects: \t");
+	        int numberOfSubjects = scanner.nextInt();
+
+	        int[] marks = new int[numberOfSubjects];
+
+	        int totalMarks = 0;
+
+	        for (int i = 0, v; i < numberOfSubjects; i++) {
+
+	            System.out.print("\nEnter the marks obtained out of 100 in Subject " + (i + 1) + ":\t ");
+
+	            v = scanner.nextInt();
+
+	            if (v <= 100) {
+	                
+	                marks[i] = v;
+
+	                
+	                totalMarks += marks[i];
+	            } else {
+	                System.out.println("\n\t!!! Please enter the valid marks !!!");
+	                i--;
+	            }
+
+	        }
+	        scanner.close();
+
+	        
+	        float averagePercentage = (float) totalMarks / numberOfSubjects;
+
+	        String grade;
+
+	        switch ((int) (averagePercentage / 10)) {
+	            case 9, 10:
+	                grade = "A";
+	                break;
+	            case 8:
+	                grade = "B";
+	                break;
+	            case 7:
+	                grade = "C";
+	                break;
+	            case 6:
+	                grade = "D";
+	                break;
+	            case 5, 4:
+	                grade = "Pass";
+	                break;
+	            default:
+	                grade = "Fail";
+	                break;
+	        }
+
+	        System.out.println("\nTotal marks: " + totalMarks + "/" + numberOfSubjects * 100);
+
+	        System.out.println("\nAverage percentage: " + averagePercentage);
+
+	        System.out.println("\nGrade: " + grade);
+
+	    }
+	}
+
+
